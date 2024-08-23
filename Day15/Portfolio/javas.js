e4624accced94e373221825d8230cd65d42bf3d2
madhavlonkar <madhavlonkar1@gmail.com>
2024-08-23 19:37:16 +0530

    Update repository: remove deleted files and add new changes

---
+++ Day15/Portfolio/javas.js
@@ -0,0 +1,11 @@
+// JavaScript can be added here for interactivity
+// Example: Scroll to sections smoothly
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
